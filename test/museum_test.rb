f50e0a0b2b34f5a47ef7b90f930108d2177bbeb5
Sienna Kopf <princess.kopf@gmail.com>
2020-05-27 09:43:37 -0600

    Fix output for reccomended exhibits based off of object ids

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -43,7 +43,7 @@ class MuseumTest < Minitest::Test
     @dmns.add_exhibit(@dead_sea_scrolls)
     @dmns.add_exhibit(@imax)
 
-    assert_equal [@dead_sea_scrolls, @gems_and_minerals], @dmns.recommend_exhibits(@patron_1)
+    assert_equal [@gems_and_minerals, @dead_sea_scrolls], @dmns.recommend_exhibits(@patron_1)
     assert_equal [@imax], @dmns.recommend_exhibits(@patron_2)
   end
 end
