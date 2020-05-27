f991cdcd64e807c0b5c73e97648c060fd55b9672
Sienna Kopf <princess.kopf@gmail.com>
2020-05-27 09:14:38 -0600

    Add test for attributes name and spending money;2A

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -10,5 +10,10 @@ class PatronTest < Minitest::Test
 
   def test_it_exists
     assert_instance_of Patron, @patron_1
-  end 
+  end
+
+  def test_it_has_attributes
+    assert_equal "Bob", @patron_1.name
+    assert_equal 20, @patron_1.spending_money
+  end
 end
