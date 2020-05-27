9ec96365f93b3c10e34446080cb1ce93ef1d3e22
Sienna Kopf <princess.kopf@gmail.com>
2020-05-27 09:18:35 -0600

    Add tests for no interests and adding interests

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -1,6 +1,5 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/exhibit'
 require './lib/patron'
 
 class PatronTest < Minitest::Test
@@ -15,5 +14,16 @@ class PatronTest < Minitest::Test
   def test_it_has_attributes
     assert_equal "Bob", @patron_1.name
     assert_equal 20, @patron_1.spending_money
+  end
+
+  def test_it_has_no_interests
+    assert_equal [], @patron_1.interests
+  end
+
+  def test_it_can_add_interests
+    @patron_1.add_interest("Dead Sea Scrolls")
+    assert_equal ["Dead Sea Scrolls"], @patron_1.interests
+    @patron_1.add_interest("Gems and Minerals")
+    assert_equal ["Dead Sea Scrolls", "Gems and Minerals"], @patron_1.interests
   end
 end
