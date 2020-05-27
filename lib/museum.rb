78f6ba50cada6989fd801b0e9437b0d777436736
Sienna Kopf <princess.kopf@gmail.com>
2020-05-27 10:45:28 -0600

    Add ticket lottery contestants method functionality

--- lib/museum.rb
+++ lib/museum.rb
@@ -29,4 +29,60 @@ class Museum
     recommendations
   end
 
+  def ticket_lottery_contestants(exhibit)
+    lottery_contestants = []
+    @patrons.each do |patron|
+      patron.interests.each do |interest|
+        if interest == exhibit.name && patron.spending_money < exhibit.cost
+          lottery_contestants << patron
+        end
+      end
+    end
+    lottery_contestants
+  end
+
+  # def patrons_by_exhibit_interest
+  #   patrons_by_interest = {}
+  #   @patrons.each do |patron|
+  #     patron.interests.each do |interest|
+  #       recommend_exhibits(patron).each do |recommendation|
+  #         if recommendation.name == interest
+  #           patrons_by_interest[recommendation] = patron
+  #         end
+  #       end
+  #     end
+  #   end
+  #   patrons_by_interest
+  #
+  # end
+
+
+
+
+
+    # patron_interests = @patrons.each do |patron|
+    #   patron.interest
+    # end
+    #
+    #
+    # sorted_exhibits = @exhibits.group_by do |exhibit|
+    #   exhibit.name == patrons.each d
+    # end
+
+
+
+    # sorted_patrons = {}
+    # patrons_with_shared_interests = []
+    # @exhibits.each do |exhibit|
+    #   @patrons.each do |patron|
+    #     patron.interests.each do |interest|
+    #       if interest == exhibit.name
+    #         patrons_with_shared_interests << patron
+    #       end
+    #     end
+    #     sorted_patrons[exhibit] = patron
+    #   end
+    # end
+    # sorted_patrons
+
 end
