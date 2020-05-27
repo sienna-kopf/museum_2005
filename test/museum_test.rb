808887dfadaf1b2d3e32e533c688cc6d34661109
Sienna Kopf <princess.kopf@gmail.com>
2020-05-27 09:35:01 -0600

    Add patron setup and test for reccomending exhibits to patrons based on interests

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -7,9 +7,17 @@ require './lib/museum'
 class MuseumTest < Minitest::Test
   def setup
     @dmns = Museum.new("Denver Museum of Nature and Science")
+
     @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
     @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
     @imax = Exhibit.new({name: "IMAX",cost: 15})
+
+    @patron_1 = Patron.new("Bob", 20)
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_1.add_interest("Gems and Minerals")
+
+    @patron_2 = Patron.new("Sally", 20)
+    @patron_2.add_interest("Imax")
   end
 
   def test_it_exists
@@ -28,5 +36,10 @@ class MuseumTest < Minitest::Test
     @dmns.add_exhibit(@dead_sea_scrolls)
     @dmns.add_exhibit(@imax)
     assert_equal [@gems_and_minerals, @dead_sea_scrolls, @imax], @dmns.exhibits
-  end 
+  end
+
+  def test_it_can_recommend_exhibits_based_on_patrons_interests
+    assert_equal [@dead_sea_scrolls, @gems_and_minerals], @dmns.recommend_exhibits(@patron_1)
+    assert_equal [@imax], @dmns.recommend_exhibits(@patron_2)
+  end
 end
