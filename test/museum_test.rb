538e3b46e86854b7bacb64e2e03f2622295b5937
Sienna Kopf <princess.kopf@gmail.com>
2020-05-27 09:51:04 -0600

    Add patrons array test to has attributes test

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -27,6 +27,7 @@ class MuseumTest < Minitest::Test
   def test_it_has_attributes
     assert_equal "Denver Museum of Nature and Science", @dmns.name
     assert_equal [], @dmns.exhibits
+    assert_equal [], @dmns.patrons 
   end
 
   def test_it_can_add_exhibits
