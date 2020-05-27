22e762a4e2159c1b1516df7657b7eea81c25b166
Sienna Kopf <princess.kopf@gmail.com>
2020-05-27 09:29:54 -0600

    Add functionality for adding exhibits

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,10 +1,14 @@
 class Museum
   attr_reader :name,
-              :exhibits 
+              :exhibits
 
   def initialize(name)
     @name = name
     @exhibits = []
   end
+
+  def add_exhibit(exhibit)
+    @exhibits << exhibit
+  end 
 
 end
