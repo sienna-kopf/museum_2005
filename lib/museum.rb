fafe5aa3eef53c865253cfba29c6634fd71bcea3
Sienna Kopf <princess.kopf@gmail.com>
2020-05-27 09:47:40 -0600

    Add functionality for recommend exhibits passing test

--- lib/museum.rb
+++ lib/museum.rb
@@ -9,6 +9,19 @@ class Museum
 
   def add_exhibit(exhibit)
     @exhibits << exhibit
-  end 
+  end
+
+  def recommend_exhibits(patron)
+    recommendations = []
+    @exhibits.each do |exhibit|
+      patron.interests.each do |interest|
+        if exhibit.name == interest
+          recommendations << exhibit
+        end
+      end
+    end
+    recommendations
+
+  end
 
 end
