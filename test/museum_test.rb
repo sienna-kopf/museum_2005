21a71d7f602ffbcb50228d87c2e17f636f20de22
Sienna Kopf <princess.kopf@gmail.com>
2020-05-27 09:29:06 -0600

    Add exhibit setup and test for adding exhibits

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -7,6 +7,9 @@ require './lib/museum'
 class MuseumTest < Minitest::Test
   def setup
     @dmns = Museum.new("Denver Museum of Nature and Science")
+    @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    @imax = Exhibit.new({name: "IMAX",cost: 15})
   end
 
   def test_it_exists
@@ -16,6 +19,14 @@ class MuseumTest < Minitest::Test
   def test_it_has_attributes
     assert_equal "Denver Museum of Nature and Science", @dmns.name
     assert_equal [], @dmns.exhibits
-  end 
+  end
 
+  def test_it_can_add_exhibits
+    @dmns.add_exhibit(@gems_and_minerals)
+    assert_equal [@gems_and_minerals], @dmns.exhibits
+
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+    assert_equal [@gems_and_minerals, @dead_sea_scrolls, @imax], @dmns.exhibits
+  end 
 end
